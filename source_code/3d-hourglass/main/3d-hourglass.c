7c6fd6bf1bfb639405952e5b486b06ef5dd012d8
bbear2 <bbear_mail@163.com>
2024-01-11 00:34:56 +0800

    create task for led cude display
    Signed-off-by: bbear2 <bbear_mail@163.com>

--- source_code/3d-hourglass/main/3d-hourglass.c
+++ source_code/3d-hourglass/main/3d-hourglass.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/FreeRTOSConfig.h"
 #include "driver/gpio.h"
 
 //active low
@@ -38,6 +41,40 @@ int l_gpios[8] = {
     CONFIG_GPIO_L_7,
 };
 
+static QueueHandle_t ledcube_data_queue = NULL;
+
+static uint8_t ledcube_data[64] = {0x0};
+
+static void led_cube_display(void* arg)
+{
+    static uint8_t data_tmp = 0x00;
+
+    //get new ledcube data from queue
+
+    //display ledcube
+    for(;;) {
+        vTaskDelay(25 / portTICK_PERIOD_MS);//update rate 1000/25=40
+        for(uint8_t level = 0;level < 8; level++) {
+	    for(uint8_t line = 0;line < 8; line++) {
+	        data_tmp = ledcube_data[level * 8 + line];
+	        for(uint8_t bit = 0;bit < 8; bit++) {
+	            gpio_set_level(rd_gpios[bit], !(data_tmp & 0x01));
+		    data_tmp = (data_tmp >> 1);
+                }
+	        gpio_set_level(l_gpios[line], 1);
+	        gpio_set_level(l_gpios[line], 0);
+	    }
+	    for(uint8_t temp = 0;temp < 8; temp++) {
+	        if(temp == level)
+	            gpio_set_level(g_gpios[temp], 0);
+	        else
+	            gpio_set_level(g_gpios[temp], 1);
+	    }
+	    vTaskDelay(25 / portTICK_PERIOD_MS);
+        }
+    }
+}
+
 void app_main(void)
 {
     //zero-initialize the config structure.
@@ -58,31 +95,31 @@ void app_main(void)
 
     int cnt = 0;
 
+    //close all the leds
     for(uint8_t i = 0;i < 8; ++i) {
 	gpio_set_level(g_gpios[i], 1);
-        gpio_set_level(l_gpios[i], 0);
         gpio_set_level(rd_gpios[i], 1);
     }
-    gpio_set_level(g_gpios[7], 0);
-    gpio_set_level(g_gpios[6], 0);
-    
-    gpio_set_level(l_gpios[0], 1);
-    gpio_set_level(l_gpios[1], 1);
-    gpio_set_level(l_gpios[2], 1);
-    gpio_set_level(l_gpios[3], 1);
-    gpio_set_level(l_gpios[4], 1);
-    gpio_set_level(l_gpios[5], 1);
-    gpio_set_level(l_gpios[6], 1);
-    gpio_set_level(l_gpios[7], 1);
+    for(uint8_t i = 0;i < 8; ++i) {
+        gpio_set_level(l_gpios[i], 1);
+    }
 
-    gpio_set_level(rd_gpios[0], 1);
-    gpio_set_level(rd_gpios[1], 1);
-    gpio_set_level(rd_gpios[2], 1);
-    gpio_set_level(rd_gpios[3], 0);
-    gpio_set_level(rd_gpios[4], 1);
-    gpio_set_level(rd_gpios[5], 1);
-    gpio_set_level(rd_gpios[6], 1);
-    gpio_set_level(rd_gpios[7], 1);
+    for(uint8_t i = 0;i < 64; ++i)
+        ledcube_data[i] = 0x00;
+
+    //create the data queue for led cube
+    ledcube_data_queue = xQueueCreate(64, sizeof(uint8_t)); 
+    //create the task for LED display 
+    xTaskCreate(led_cube_display, "led_cube_display", 2048, NULL, 10, NULL);
+
+    ledcube_data[0] = 0x01;
+    ledcube_data[9] = 0x02;
+    ledcube_data[18] = 0x04;
+    ledcube_data[27] = 0x08;
+    ledcube_data[36] = 0x10;
+    ledcube_data[45] = 0x20;
+    ledcube_data[54] = 0x40;
+    ledcube_data[63] = 0x80;
 
     while(1) {
 	vTaskDelay(500 / portTICK_PERIOD_MS);
